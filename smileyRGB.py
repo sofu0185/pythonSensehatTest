2c0778c03bd293b627f0b03c31c132fe4f2b70ac
sofu0185 <44866491+sofu0185@users.noreply.github.com>
2019-04-24 14:12:59 +0200

    Update smileyRGB.py

--- smileyRGB.py
+++ smileyRGB.py
@@ -21,20 +21,31 @@ r = 255
 g = 0
 b = 0
 
+print(sense.get_gyroscope()["yaw"])
 
 while(True):
+	rotation = sense.get_gyroscope()["yaw"]
+	if 45 <= rotation < 135:
+		sense.set_rotation(0)
+	elif 135 <= rotation < 225:
+		sense.set_rotation(270)
+	elif 225 <= rotation < 315:
+		sense.set_rotation(180)
+	elif 315 <= rotation <= 360 or 0 <= rotation < 45:
+		sense.set_rotation(90)
+		
 	if r == 255 and g < 255 and b == 0:
-		g += 1
+		g += 5
 	elif r > 0 and g == 255 and b == 0:
-		r -= 1
+		r -= 5
 	elif r == 0 and g == 255 and b < 255:
-		b += 1
+		b += 5
 	elif r == 0 and g > 0 and b == 255:
-		g -= 1
+		g -= 5
 	elif r < 255 and g == 0 and b == 255:
-		r += 1
+		r += 5
 	elif r == 255 and g == 0 and b > 0:
-		b -= 1
+		b -= 5
 	
 	color = (r, g, b)
 	
