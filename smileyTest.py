4995c832711fbb2c5419a5b7d2b2e8fc530865d2
sofu0185 <44866491+sofu0185@users.noreply.github.com>
2019-04-24 12:58:29 +0200

    Create smileyTest.py

---
+++ smileyTest.py
@@ -0,0 +1,37 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+def setSmiley(color = (255, 0, 0)):
+  X = color
+  O = (0, 0, 0)
+  pixels = [
+   O, O, O, O, O, O, O, O,
+   O, X, X, O, O, X, X, O,
+   O, X, X, O, O, X, X, O,
+   O, O, O, O, O, O, O, O,
+   O, O, O, O, O, O, O, O,
+   O, X, O, O, O, O, X, O,
+   O, O, X, X, X, X, O, O,
+   O, O, O, O, O, O, O, O
+  ]
+  sense.set_pixels(pixels)
+
+
+while(True):
+  sense.set_rotation(0)
+  setSmiley()
+  sleep(.3)
+
+  sense.set_rotation(90)
+  setSmiley((0, 255, 0))
+  sleep(.3)
+
+  sense.set_rotation(180)
+  setSmiley((0, 0, 255))
+  sleep(.3)
+
+  sense.set_rotation(270)
+  setSmiley((255, 255, 255))
+  sleep(.3)
