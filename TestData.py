0f180b7a902da008d2ed01b1a98b858dfb919f4c
sofu0185 <44866491+sofu0185@users.noreply.github.com>
2019-05-03 13:00:43 +0200

    Update TestData.py

--- TestData.py
+++ TestData.py
@@ -1,8 +1,7 @@
 from sense_hat import SenseHat
-from time import sleep
 
 sense = SenseHat()
 
 while True:
-	print("x: {x}, y: {y}, z: {z}".format(**sense.get_accelerometer_raw())
-
+	raw = sense.get_accelerometer_raw()
+	print("x: {x}, y: {y}, z: {z}".format(**raw))
